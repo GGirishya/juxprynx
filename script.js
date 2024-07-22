e9766c054cf387f0df48832fb1582def6ad05333
Guillaume Girishya <143455392+GGirishya@users.noreply.github.com>
2024-07-22 02:52:03 -0500

    Added a signature

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
   var welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
   welcomeModal.show();
 });
+const asciiArt = `
+ _    ____      _____                ____           _        __       _   _ 
+| |  / ___|    |  ___|__  _ __      / ___|_ __ __ _| |_ ___ / _|_   _| | | |
+| | | |  _     | |_ / _ \\| '__|    | |  _| '__/ _\` | __/ _ \\ |_| | | | | | |
+|_| | |_| |    |  _| (_) | |       | |_| | | | (_| | ||  __/  _| |_| | | |_|
+(_)  \\____|    |_|  \\___/|_|        \\____|_|  \\__,_|\\__\\___|_|  \\__,_|_| (_)
+                                    ♥
+`;
+
+console.log(asciiArt);
 console.log(" Gracie and G developed this. Any suggestions can be sent to our linkedIn or Git");
 console.log(" G; LinkedIN: Guillaume Girishya");
 console.log(" Gracie; LinkedIN: Grace Akintoye");
